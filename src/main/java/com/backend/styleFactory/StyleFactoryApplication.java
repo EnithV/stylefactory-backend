fbfa3788ee0be9ff3ff9ffaf2ea4168393a0027f
marcelaW40k <ardilamarcela42@gmail.com>
2026-05-25 12:35:28 -0500

    Estructura con spring boot

---
+++ src/main/java/com/backend/styleFactory/StyleFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.backend.styleFactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StyleFactoryApplication.class, args);
+	}
+
+}
