015c7d4ccf2b1236232b73ea514c6caf9dd9be60
Luis Rodriguez <luedropa@gmail.com>
2026-05-26 15:03:44 -0500

    Arreglando algunos detalles

--- src/main/java/com/backend/styleFactory/repository/UsuarioRespository.java
+++
@@ -1,7 +0,0 @@
-package com.backend.styleFactory.repository;
-
-import com.backend.styleFactory.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRespository extends JpaRepository<Usuario, Long> {
-}
